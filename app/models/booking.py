3c657c250dd41dd405a90362476265d7370aeda8
Omii04 <omi131004@gmail.com>
2026-06-30 18:44:28 +0530

    update and adding new files

--- app/models/booking.py
+++ app/models/booking.py
@@ -1,26 +1,36 @@
 from datetime import datetime
+
 from app.extensions import db
 
 
 class Booking(db.Model):
-
     __tablename__ = "bookings"
 
     id = db.Column(db.Integer, primary_key=True)
 
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey("users.id"),
-                        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
 
-    bus_id = db.Column(db.Integer,
-                       db.ForeignKey("buses.id"),
-                       nullable=False)
+    bus_id = db.Column(
+        db.Integer,
+        db.ForeignKey("buses.id"),
+        nullable=False
+    )
 
-    seat_number = db.Column(db.Integer,
-                            nullable=False)
+    seat_number = db.Column(
+        db.Integer,
+        nullable=False
+    )
 
-    booking_date = db.Column(db.DateTime,
-                             default=datetime.utcnow)
+    booking_date = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
 
-    status = db.Column(db.String(20),
-                       default="Booked")
+    status = db.Column(
+        db.String(20),
+        default="Booked"
+    )
